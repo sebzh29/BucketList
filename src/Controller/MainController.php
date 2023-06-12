008ffa3aa12587cfc8ead42904afcdcb0ea3149e
Sébastien Glippa <sebzh29@outlook.com>
2023-06-12 20:29:14 +0200

    premier commit BucketList TP cours

---
+++ src/Controller/MainController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'app_main')]
+    public function home(): Response
+    {
+
+        return $this->render('main/home.html.twig', [
+            'controller_name' => 'Bucket List',
+        ]);
+    }
+}
